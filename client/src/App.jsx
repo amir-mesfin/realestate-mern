6240940aaf71f10968848194a400b64f1b2ae8f1
abushe136 <amir.mesfin136@gmail.com>
2025-07-30 22:07:09 +0300

    add Header component and update routes; include react-icons dependency

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import {BrowserRouter,Route,Routes} from 'react-router-dom';
 import Home from './page/Home';
 import Signin from './page/Signin';
 import Signup from './page/Signup';
 import About from './page/About';
 import Profile from './page/Profile';
+import Header from './component/Header';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
+     <Header />
        <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
               <Route path="/sign-in" element={<Signin />} />
               <Route path="/sign-up" element={<Signup />} />
               <Route path="/about" element={<About />} />
