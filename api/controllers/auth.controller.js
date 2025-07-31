907d4c269f26e2151bafd943551635d68ba64e82
abushe136 <amir.mesfin136@gmail.com>
2025-07-31 11:00:41 +0300

    add authentication routes and user signup functionality with bcryptjs

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+import User from '../models/user.model.js'
+import bcryptjs from 'bcryptjs';
+
+export const signup = async (req,res)=>{
+   // console.log(req.body);
+         const {username, email, password} = req.body;
+         const hashePassword = bcryptjs.hashSync(password,10);
+         const newUser = new User({username, email, password:hashePassword});
+   try{
+         await newUser.save();
+         res.status(201).json("user created successfully");
+   }catch(err){
+        res.status(500).json(err.message);
+   }
+   
+
+}
--- api/index.js
+++ api/index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import connectDB from './config/db.js';
 import userRouter from './routes/userRouter.js'
+import authRouter from './routes/auth.route.js'
+
+
 const app = express();
 const PORT = process.env.PORT || 3000
 
-
+app.use(express.json());
 app.use("/api",userRouter);
-
+app.use("/api/auth",authRouter )
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
