3a5a9de4128b5c1a78f068bbd2cef81f241f721b
abushe136 <amir.mesfin136@gmail.com>
2025-08-04 16:23:48 +0300

    worke on profile page

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {FaSearch} from 'react-icons/fa';
+import { useSelector } from 'react-redux';
+
 export default function Header() {
+  const {user} = useSelector(state=>state.user);
   return (
     <header className='bg-slate-200 shadow-md ' >
        <div className='flex  justify-between items-center max-w-6xl mx-auto p-3'>
