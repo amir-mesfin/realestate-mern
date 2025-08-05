dd3654332b1d8f92f3a103d1299eeb1a2088aeeb
abushe136 <amir.mesfin136@gmail.com>
2025-08-05 13:08:07 +0300

    worke on the functionality of profile page

--- client/src/page/Profile.jsx
+++ client/src/page/Profile.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import { useSelector } from 'react-redux'
+
 
 export default function Profile() {
 const {currentUser} = useSelector((state)=>state.user);
 const avatarURL = currentUser?.avatar?.includes("googleusercontent") && !currentUser.avatar.includes("=")
   ? currentUser.avatar + "=s96-c"
   : currentUser?.avatar || "https://www.w3schools.com/howto/img_avatar.png";
+
+  const fileRef = useRef(null)
   return (
     <div className='max-w-lg mx-auto'>
      <h1 className='text-3xl text-center font-semibold my-9'>Profile</h1>
      <form  className='flex flex-col gap-4'>
-      <img src={avatarURL}
+      <input  ref={fileRef}
+      hidden
+       className='border-0 p-4 focus:outline-blue-300  rounded-lg bg-white '
+      type="file" />
+      <img  onClick={()=> fileRef.current.click()}
+      src={avatarURL}
       alt="profile img" 
       className='rounded-full h-39 w-39 object:cover cursor-pointer mt-2 self-center ' />
       <input type="text"
