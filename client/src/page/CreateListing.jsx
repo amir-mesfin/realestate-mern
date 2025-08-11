ca3a696102ac363e167d02633b01be83ea98ec6f
abushe136 <amir.mesfin136@gmail.com>
2025-08-11 16:02:02 +0300

    worke on the create list page  3 input

--- client/src/page/CreateListing.jsx
+++ client/src/page/CreateListing.jsx
@@ -2,8 +2,28 @@ import React from 'react'
 
 export default function CreateListing() {
   return (
-    <div>
-      create listing
-    </div>
+     <main>
+      <h1 className='text-3xl text-center font-semibold my-9'>Create Listing</h1>
+      <form className='flex flex-col sm:flex-row' >
+          <div className='flex flex-col gap-4'>
+            <input type="text"
+                   placeholder='name' 
+                   className='border-0 p-4  focus:outline-blue-300 rounded-lg bg-white'  id="name"
+                   maxLength={67}
+                   minLength={5}
+                   required/>
+            <textarea type="text"
+                   placeholder='Description' 
+                   className='border-0 p-4  focus:outline-blue-300 rounded-lg bg-white' 
+                    id="description"
+                   required/>
+            <input type="text"
+                   placeholder='Address' 
+                   className='border-0 p-4  focus:outline-blue-300 rounded-lg bg-white'  id="address"
+                   required/>
+          
+          </div>
+      </form>
+     </main>
   )
 }
