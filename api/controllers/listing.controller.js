4f48b7ae90ab6edff7096d44e2b8eb9a13fefe16
abushe136 <amir.mesfin136@gmail.com>
2025-08-14 16:44:03 +0300

    feat: add deleteListing functionality to manage user listings

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from '../models/listing.model.js';
+import { ErrorHandler } from '../utils/error.js';
 
 export  const createListing  = async (req,res,next)=>{
    try{
@@ -8,3 +9,19 @@ export  const createListing  = async (req,res,next)=>{
     next(err);
    }
 } 
+
+export const deleteListing =  async(req,res,next)=>{
+   try{
+        const listings = await Listing.findById(req.params.id);
+        if(!listings) return next(ErrorHandler(404, 'Listing not Found'))
+        
+         if(req.params.id !== listings.userRef){
+            return next(ErrorHandler(401 , 'You are not authorized to delete this listing'));
+         }
+         await  Listing.findByIdAndDelete(req.params.id);
+         
+
+   }catch(err){
+      next(err);
+   }
+}
