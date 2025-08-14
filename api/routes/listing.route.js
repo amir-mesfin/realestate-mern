0e7c597b2aa3cad45853acabaed19cdab40c7b6c
abushe136 <amir.mesfin136@gmail.com>
2025-08-15 01:16:25 +0300

    fix: correct deleteListing route path in listing.route.js

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 
 router.post('/create',verifyToken, createListing);
-router.delete('delete/:id', verifyToken, deleteListing);
+router.delete('/delete/:id', verifyToken, deleteListing);
 
 
 export  default router;
