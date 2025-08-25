5d141c85e9c4632a9086f6bd0aec115f01853721
abushe136 <amir.mesfin136@gmail.com>
2025-08-25 14:03:02 +0300

    correction for  config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
-        target: 'http://localhost:5175',
-        secure:false,
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5175', // dev backend
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
-  plugins: [
-    react(),
-    tailwindcss(),],
+  plugins: [react()],
 })
